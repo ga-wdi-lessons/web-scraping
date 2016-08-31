bf30c2d7fc4c1f72f244b17f37c151ce084b16ff
Jesse Shawl <jesse@jshawl.com>
2016-08-31 09:38:23 -0400

    remove solution

--- code/02-craigslist/app.py
+++ code/02-craigslist/app.py
@@ -1,17 +1,1 @@
-#!/usr/bin/env python
 
-import urllib
-from bs4 import BeautifulSoup
-
-baseurl = 'http://localhost:8000'
-url = baseurl + '/search/cpg'
-html = urllib.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
-for link in soup.findAll('a',{'class':'hdrlnk'}):
-  gigurl = baseurl + link['href']
-  gig = urllib.urlopen(gigurl).read()
-  gigsoup = BeautifulSoup(gig,'html.parser')
-  title = gigsoup.find('span',{'id':'titletextonly'}).renderContents()
-  print "TITLE:", title
-  body = gigsoup.find('section',{'id':'postingbody'}).renderContents()
-  print "BODY:", body
